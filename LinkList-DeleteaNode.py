330575430fa83ed19157f865f7206f7cda3bc50c
TejasJogi <tejasjogi1995@gmail.com>
2023-07-08 22:06:13 +0530

    SinglyLinkedList class created

--- LinkList-DeleteaNode.py
+++ LinkList-DeleteaNode.py
@@ -45,6 +45,11 @@
 
 
 class Node:
-    def __init__(self, head):
-        self.head = head
+    def __init__(self, data):
+        self.data = data
         self.next = None
+
+class SinglyLinkedList:
+    def __init__(self,):
+        self.head = None
+        self.tail = None
