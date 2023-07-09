6c58446d337163adadb3a6e1b0358b39bf45d501
TejasJogi <tejasjogi1995@gmail.com>
2023-07-09 17:56:40 +0530

    SinglyLinedList class created

--- LinkList-reversePrint.py
+++ LinkList-reversePrint.py
@@ -72,3 +72,8 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
