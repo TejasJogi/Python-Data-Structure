3794d80a862fc15e91219fe38a5c0138ed16c8eb
TejasJogi <tejasjogi1995@gmail.com>
2023-07-13 19:09:31 +0530

    Inputs

--- LinkedList-CompareTwoLinkedList.py
+++ LinkedList-CompareTwoLinkedList.py
@@ -60,3 +60,26 @@ class SinglyLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
+
+tests = int(input())
+
+for tests_itr in range(tests):
+    llist1_count = int(input())
+
+    llist1 = SinglyLinkedList()
+
+    for _ in range(llist1_count):
+        llist1_item = int(input())
+        llist1.insert_node(llist1_item)
+        
+    llist2_count = int(input())
+
+    llist2 = SinglyLinkedList()
+
+    for _ in range(llist2_count):
+        llist2_item = int(input())
+        llist2.insert_node(llist2_item)
+
+    result = compare_lists(llist1.head, llist2.head)
+
+    print(result)
