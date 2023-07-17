f5aac84722f4d0fbfc07629d31fd02ae49f0db25
TejasJogi <tejasjogi1995@gmail.com>
2023-07-17 20:27:48 +0530

    insert_node added in SinglyLinkedList class

--- LinkedList-MergeTwoSortedLinkedList.py
+++ LinkedList-MergeTwoSortedLinkedList.py
@@ -55,28 +55,39 @@ class SinglyLinkedList():
         self.head = None
         self.tail = None
 
+    def insert_node(self, data):
+        node = Node(data)
+
+        if self.head == None:
+            self.head = node
+        else:
+            self.tail.next = node
+        self.tail = node
+
 def mergeLists(head1, head2):
     pass
 
-tests = int(input())
+if __name__ == '__main__':
 
-for tests_itr in range(tests):
-    llist1_count = int(input())
+    tests = int(input())
 
-    llist1 = SinglyLinkedList()
+    for tests_itr in range(tests):
+        llist1_count = int(input())
 
-    for _ in range(llist1_count):
-        llist1_item = int(input())
-        llist1.insert_node(llist1_item)
-        
-    llist2_count = int(input())
+        llist1 = SinglyLinkedList()
 
-    llist2 = SinglyLinkedList()
+        for _ in range(llist1_count):
+            llist1_item = int(input())
+            llist1.insert_node(llist1_item)
+            
+        llist2_count = int(input())
 
-    for _ in range(llist2_count):
-        llist2_item = int(input())
-        llist2.insert_node(llist2_item)
+        llist2 = SinglyLinkedList()
 
-    llist3 = mergeLists(llist1.head, llist2.head)
+        for _ in range(llist2_count):
+            llist2_item = int(input())
+            llist2.insert_node(llist2_item)
 
-    print_singly_linked_list(llist3)
+        llist3 = mergeLists(llist1.head, llist2.head)
+
+        print_singly_linked_list(llist3)
