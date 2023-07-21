339f87d8bcbb99a65af833394b3bd486d4c8710f
TejasJogi <tejasjogi1995@gmail.com>
2023-07-21 22:40:44 +0530

    input method added for SinglyLinkedList

--- LinkedList-CycleDetection.py
+++ LinkedList-CycleDetection.py
@@ -63,3 +63,32 @@ class SinglyLinkedList:
             self.tail.next = node
 
         self.tail = node
+
+tests = int(input())
+
+for tests_itr in range(tests):
+    index = int(input())
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for _ in range(llist_count):
+        llist_item = int(input())
+        llist.insert_node(llist_item)
+
+    extra = Node(-1);
+    temp = llist.head;
+
+    for i in range(llist_count):
+        if i == index:
+            extra = temp
+
+        if i != llist_count-1:
+            temp = temp.next
+
+    temp.next = extra
+
+    result = has_cycle(llist.head)
+
+    print(str(int(result)) + '\n')
