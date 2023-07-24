f2700ea9a382be8453ccf61e4bd8e673b5ba7237
TejasJogi <tejasjogi1995@gmail.com>
2023-07-24 20:09:40 +0530

    inputs added

--- LinkedList-FindMergePoint.py
+++ LinkedList-FindMergePoint.py
@@ -87,3 +87,38 @@ class SinglyLinkedList:
             self.tail.next = node
         
         self.tail = node
+
+def findMergeNode(head1, head2):
+    pass
+
+tests = int(input())
+
+for tests_itr in range(tests):
+    index = int(input())
+
+    llist1_count = int(input())
+
+    llist1 = SinglyLinkedList()
+
+    for _ in range(llist1_count):
+        llist1_item = int(input())
+        llist1.insert_node(llist1_item)
+        
+    llist2_count = int(input())
+
+    llist2 = SinglyLinkedList()
+
+    for _ in range(llist2_count):
+        llist2_item = int(input())
+        llist2.insert_node(llist2_item)
+        
+    ptr1 = llist1.head;
+    ptr2 = llist2.head;
+
+    for i in range(llist1_count):
+        if i < index:
+            ptr1 = ptr1.next
+            
+    for i in range(llist2_count):
+        if i != llist2_count-1:
+            ptr2 = ptr2.next
