748770a48e80382c9bb969f1027683cee9d878a3
TejasJogi <tejasjogi1995@gmail.com>
2023-08-16 21:29:47 +0530

    Task and input methods added for Postorder Traversal

---
+++ Tree-PostorderTraversal.py
@@ -0,0 +1,24 @@
+# Complete the postOrder function in the editor below. It received 1 parameter: a pointer to the root of a binary tree. It must print the values in the tree's postorder traversal as a single line of space-separated values.
+
+# Input Format
+
+# Our test code passes the root node of a binary tree to the postOrder function.
+
+# Output Format
+
+# Print the tree's postorder traversal as a single line of space-separated values.
+
+# Sample Input
+
+#      1
+#       \
+#        2
+#         \
+#          5
+#         /  \
+#        3    6
+#         \
+#          4
+# Sample Output
+
+# 4 3 6 5 2 1 
