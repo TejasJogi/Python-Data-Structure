a6d921f9ddce39187ec1e491d8e98677932e7ee1
TejasJogi <tejasjogi1995@gmail.com>
2023-08-17 13:58:03 +0530

    Node class added for BinarySearchTree

--- Trees-PostorderTraversal.py
+++ Trees-PostorderTraversal.py
@@ -23,6 +23,16 @@
 
 # 4 3 6 5 2 1 
 
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+    
+    def __str__(self):
+        return str(self.info)
+
 tree = BinarySearchTree()
 t = int(input())
 
