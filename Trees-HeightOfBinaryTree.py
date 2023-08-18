24c4d2650d86ed1cfa9966ad6d903d29bbcb4974
TejasJogi <tejasjogi1995@gmail.com>
2023-08-18 20:53:50 +0530

    Task and inputs methods added for Binary Search Tree

---
+++ Trees-HeightOfBinaryTree.py
@@ -0,0 +1,42 @@
+# The height of a binary tree is the number of edges between the tree's root and its furthest leaf. For example, the following binary tree is of height 2:
+
+# Function Description
+
+# Complete the getHeight or height function in the editor. It must return the height of a binary tree as an integer.
+
+# getHeight or height has the following parameter(s):
+
+# root: a reference to the root of a binary tree.
+
+# Note -The Height of binary tree with single node is taken as zero.
+
+# Input Format
+
+# The first line contains an integer n, the number of nodes in the tree.
+# Next line contains n space separated integer where ith integer denotes node[i].data.
+
+# Note: Node values are inserted into a binary search tree before a reference to the tree's root node is passed to your function. In a binary search tree, all nodes on the left branch of a node are less than the node value. All values on the right branch are greater than the node value.
+
+# Output Format
+
+# Your function should return a single integer denoting the height of the binary tree.
+
+# Sample Input
+
+# 7
+# 4 2 1 3 7 6 5
+
+# Sample Output
+
+# 3
+
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+print(height(tree.root))
