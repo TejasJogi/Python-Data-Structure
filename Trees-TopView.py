463d492775a8e1b8f4408ebed1fc00d0f258f4f3
TejasJogi <tejasjogi1995@gmail.com>
2023-08-23 22:26:19 +0530

    Add class Node for BinarySearchTree

---
+++ Trees-TopView.py
@@ -0,0 +1,66 @@
+# Given a pointer to the root of a binary tree, print the top view of the binary tree.
+
+# The tree as seen from the top the nodes, is called the top view of the tree.
+
+# For example :
+
+#    1
+#     \
+#      2
+#       \
+#        5
+#       /  \
+#      3    6
+#       \
+#        4
+
+# Top View : 1 -> 2 -> 5 -> 6
+
+# Complete the function  and print the resulting values on a single line separated by space.
+
+# Input Format
+
+# You are given a function,
+
+# void topView(node * root) {
+
+# }
+
+# Output Format
+
+# Print the values on a single line separated by space.
+
+# Sample Input
+
+#    1
+#     \
+#      2
+#       \
+#        5
+#       /  \
+#      3    6
+#       \
+#        4
+
+# Sample Output
+
+# 1 2 5 6
+
+# Explanation
+
+#    1
+#     \
+#      2
+#       \
+#        5
+#       /  \
+#      3    6
+#       \
+#        4
+
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
