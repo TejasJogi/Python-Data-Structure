8049b38dee9632db7ea4ad303214cbc9956e37f7
TejasJogi <tejasjogi1995@gmail.com>
2023-09-04 20:41:55 +0530

    Input methods

--- Trees-HuffmanDecoding.py
+++ Trees-HuffmanDecoding.py
@@ -97,3 +97,32 @@ def dfs_hidden(obj, already):
         
     dfs_hidden(obj.right, already + "1")
     dfs_hidden(obj.left, already + "0")
+
+
+ip = input()
+freq = {}#maps each character to its frequency
+
+cntr = 0
+
+for ch in ip:
+    if(freq.get(ch) == None):
+        freq[ch] = 1
+    else:
+        freq[ch]+=1
+
+root = huffman_hidden()#contains root of huffman tree
+
+code_hidden = {}#contains code for each object
+
+dfs_hidden(root, "")
+
+if len(code_hidden) == 1:#if there is only one character in the i/p
+    for key in code_hidden:
+        code_hidden[key] = "0"
+
+toBeDecoded = ""
+
+for ch in ip:
+   toBeDecoded += code_hidden[ch]
+
+decodeHuff(root, toBeDecoded)
