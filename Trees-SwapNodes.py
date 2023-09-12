54da3c25c511b20863555831e47bf142c327fa64
TejasJogi <tejasjogi1995@gmail.com>
2023-09-12 19:18:02 +0530

    Node class added for cresting  a nodes for BinarySearchTree

--- Trees-SwapNodes.py
+++ Trees-SwapNodes.py
@@ -148,3 +148,12 @@
 #   \        /   \          /           /   \              \       /   \  
 #    9      10   11        9           11   10              9     10   11 
 
+class Node:
+    def __init__(self, info):
+        self.info = info
+        self.left = None
+        self.right = None
+        self.level = None
+
+    def __str__(self):
+        return str(self.info)
